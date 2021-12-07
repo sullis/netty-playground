62297e91d273d5b5a55428c669a98055e6a96f00
Sean C. Sullivan <github@seansullivan.com>
2021-12-07 15:15:24 -0800

    add IoUringTest.java

---
+++ src/test/java/io/github/sullis/netty/playground/IoUringTest.java
@@ -0,0 +1,20 @@
+package io.github.sullis.netty.playground;
+
+import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.junit.jupiter.api.Test;
+
+import io.netty.incubator.channel.uring.IOUring;
+
+public class IoUringTest {
+    @Test
+    public void testUringTransport() {
+        System.out.println("Linux: " + IS_OS_LINUX);
+        if (IS_OS_LINUX) {
+            final Throwable cause = IOUring.unavailabilityCause();
+            if (cause != null) {
+                System.err.println("unavailabilityCause: " + ExceptionUtils.getStackTrace(cause));
+            }
+        }
+    }
+}
