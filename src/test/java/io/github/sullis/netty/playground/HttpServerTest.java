1c40d3c83197d47478b0a8ec8db995af49f6cf80
sullis <github@seansullivan.com>
2023-01-20 20:05:20 -0800

    Apache HttpClient brotli

--- src/test/java/io/github/sullis/netty/playground/HttpServerTest.java
+++ src/test/java/io/github/sullis/netty/playground/HttpServerTest.java
@@ -2,9 +2,19 @@ package io.github.sullis.netty.playground;
 
 import com.aayushatharva.brotli4j.Brotli4jLoader;
 import io.netty.handler.codec.compression.Brotli;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.entity.DecompressingEntity;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HttpServerTest {
@@ -18,6 +28,16 @@ public class HttpServerTest {
     public void startStop() throws Exception {
         HttpServer server = new HttpServer();
         server.start();
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://localhost:8080/");
+        httpGet.setHeader("Accept-Encoding", "br");
+        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+        DecompressingEntity responseEntity = (DecompressingEntity) httpResponse.getEntity();
+        assertEquals("text/plain", responseEntity.getContentType());
+        assertEquals(-1, responseEntity.getContentLength());
+        String text = EntityUtils.toString(responseEntity);
+        assertEquals("foo", text);
         server.stop();
     }
 }
