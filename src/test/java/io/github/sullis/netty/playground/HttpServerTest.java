cc9faf7069436625b95536bf6bdbe2091d8a79dc
sullis <github@seansullivan.com>
2023-01-20 19:44:44 -0800

    add Brotli assertions

--- src/test/java/io/github/sullis/netty/playground/HttpServerTest.java
+++ src/test/java/io/github/sullis/netty/playground/HttpServerTest.java
@@ -1,7 +1,19 @@
 package io.github.sullis.netty.playground;
 
+import com.aayushatharva.brotli4j.Brotli4jLoader;
+import io.netty.handler.codec.compression.Brotli;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class HttpServerTest {
+    @BeforeEach
+    public void beforeEach() {
+        assertTrue(Brotli4jLoader.isAvailable());
+        assertTrue(Brotli.isAvailable());
+    }
+
     @Test
     public void startStop() throws Exception {
         HttpServer server = new HttpServer();
