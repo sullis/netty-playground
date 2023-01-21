43bb11dd8d0043f36ff8309fa1275b7c4b483336
Sean C. Sullivan <github@seansullivan.com>
2023-01-21 11:32:44 -0800

    drop OkHttp library

--- src/test/java/io/github/sullis/netty/playground/HttpServerTest.java
+++ src/test/java/io/github/sullis/netty/playground/HttpServerTest.java
@@ -1,13 +1,10 @@
 package io.github.sullis.netty.playground;
 
 import com.aayushatharva.brotli4j.Brotli4jLoader;
+import com.aayushatharva.brotli4j.decoder.BrotliInputStream;
 import com.aayushatharva.brotli4j.decoder.DecoderJNI;
 import com.aayushatharva.brotli4j.decoder.DirectDecompress;
 import io.netty.handler.codec.compression.Brotli;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.brotli.BrotliInterceptor;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
@@ -17,6 +14,8 @@ import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -52,25 +51,9 @@ public class HttpServerTest {
         assertEquals("br", httpResponse.getFirstHeader("content-encoding").getValue());
         HttpEntity responseEntity = httpResponse.getEntity();
         assertEquals("text/plain", responseEntity.getContentType());
-        byte[] byteArray = EntityUtils.toByteArray(responseEntity);
-        DirectDecompress result = DirectDecompress.decompress(byteArray);
-        assertEquals(DecoderJNI.Status.DONE, result.getResultStatus());
-    }
-
-    @Test
-    public void brotliWithOkHttp() throws Exception {
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(BrotliInterceptor.INSTANCE)
-                .build();
-        Request request = new Request.Builder()
-                .url(defaultUrl)
-                .build();
-        Response response = client.newCall(request).execute();
-        // assertEquals("br", response.header("content-encoding"));
-        assertEquals("text/plain", response.header("content-type"));
-        assertEquals("Hello world", response.body().string());
-        // byte[] byteArray = response.body().bytes();
-        // DirectDecompress result = DirectDecompress.decompress(byteArray);
-        // assertEquals(DecoderJNI.Status.DONE, result.getResultStatus());
+        BrotliInputStream brotliInputStream = new BrotliInputStream(responseEntity.getContent());
+        String text = new String(brotliInputStream.readAllBytes(), StandardCharsets.UTF_8);
+        assertEquals("Hello world", text);
+        brotliInputStream.close();
     }
 }
