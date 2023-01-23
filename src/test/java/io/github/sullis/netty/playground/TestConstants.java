5d2f07bd9862d4a60535fef3e41f37d3bdc39d28
Sean C. Sullivan <github@seansullivan.com>
2023-01-22 20:21:55 -0800

    update TestConstants.CONTENT

--- src/test/java/io/github/sullis/netty/playground/TestConstants.java
+++ src/test/java/io/github/sullis/netty/playground/TestConstants.java
@@ -4,6 +4,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class TestConstants {
-    public static final String CONTENT = "Hello Hello Hello";
+    public static final String CONTENT = "Hello Hello Hello Hello Hello";
     public static final Charset CHARSET = StandardCharsets.UTF_8;
 }
