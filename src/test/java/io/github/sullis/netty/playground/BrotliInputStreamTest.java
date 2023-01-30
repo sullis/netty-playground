db9a352ce2d5c52b650ce623917f81f1fa1e8668
Sean C. Sullivan <github@seansullivan.com>
2023-01-30 10:31:53 -0800

    remove unused import

--- src/test/java/io/github/sullis/netty/playground/BrotliInputStreamTest.java
+++ src/test/java/io/github/sullis/netty/playground/BrotliInputStreamTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
