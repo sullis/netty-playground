e144c67fca839afed7caca208a9f4ed6ebc5ed66
Sean C. Sullivan <github@seansullivan.com>
2023-11-27 04:54:42 -0800

    improve OpenSslTest

--- src/test/java/io/github/sullis/netty/playground/OpenSslTest.java
+++ src/test/java/io/github/sullis/netty/playground/OpenSslTest.java
@@ -1,15 +1,22 @@
 package io.github.sullis.netty.playground;
 
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.handler.ssl.OpenSsl;
 import io.netty.handler.ssl.OpenSslClientContext;
+import io.netty.handler.ssl.OpenSslEngine;
 import io.netty.handler.ssl.OpenSslServerContext;
 import io.netty.handler.ssl.OpenSslSessionContext;
 import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.ssl.SslProvider;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.net.ssl.SSLParameters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,8 +30,40 @@ class OpenSslTest {
     @Test
     void testBoringSsl() throws Exception {
         assertThat(OpenSsl.versionString()).isEqualTo("BoringSSL");
+        assertThat(OpenSsl.supportsKeyManagerFactory()).isTrue();
         assertTrue(SslProvider.isAlpnSupported(SslProvider.OPENSSL));
         assertTrue(SslProvider.isTlsv13Supported(SslProvider.OPENSSL));
+    }
+
+    @Test
+    void testAvailableCipherSuites() {
+        assertThat(OpenSsl.availableOpenSslCipherSuites())
+                .containsExactly( "ECDHE-ECDSA-AES128-GCM-SHA256",
+                "ECDHE-RSA-AES128-GCM-SHA256",
+                "ECDHE-ECDSA-AES256-GCM-SHA384",
+                "ECDHE-RSA-AES256-GCM-SHA384",
+                "ECDHE-ECDSA-CHACHA20-POLY1305",
+                "ECDHE-RSA-CHACHA20-POLY1305",
+                "ECDHE-PSK-CHACHA20-POLY1305",
+                "ECDHE-ECDSA-AES128-SHA",
+                "ECDHE-RSA-AES128-SHA",
+                "ECDHE-PSK-AES128-CBC-SHA",
+                "ECDHE-ECDSA-AES256-SHA",
+                "ECDHE-RSA-AES256-SHA",
+                "ECDHE-PSK-AES256-CBC-SHA",
+                "AES128-GCM-SHA256",
+                "AES256-GCM-SHA384",
+                "AES128-SHA",
+                "PSK-AES128-CBC-SHA",
+                "AES256-SHA",
+                "PSK-AES256-CBC-SHA",
+                "DES-CBC3-SHA",
+                "TLS_AES_128_GCM_SHA256",
+                "TLS_AES_256_GCM_SHA384",
+                "TLS_CHACHA20_POLY1305_SHA256",
+                "AEAD-AES128-GCM-SHA256",
+                "AEAD-AES256-GCM-SHA384",
+                "AEAD-CHACHA20-POLY1305-SHA256");
     }
 
     @Test
@@ -34,6 +73,30 @@ class OpenSslTest {
         assertThat(sessionCtx.getSessionCacheSize()).isEqualTo(20480);
         assertThat(sessionCtx.getSessionTimeout()).isEqualTo(300);
         assertThat(sessionCtx.isSessionCacheEnabled()).isEqualTo(true);
+        assertEquals(1, clientCtx.refCnt());
+        SslHandler handler = clientCtx.newHandler(ByteBufAllocator.DEFAULT);
+        assertNull(handler.applicationProtocol());
+        OpenSslEngine engine = (OpenSslEngine) handler.engine();
+        assertThat(engine.getEnabledProtocols()).containsExactly("SSLv2Hello", "TLSv1.2", "TLSv1.3");
+        assertThat(engine.getSupportedProtocols()).containsExactly("SSLv2Hello", "TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3");
+        SSLParameters sslParameters = engine.getSSLParameters();
+        assertThat(sslParameters.getNeedClientAuth()).isFalse();
+        assertThat(sslParameters.getWantClientAuth()).isFalse();
+        assertThat(sslParameters.getEnableRetransmissions()).isTrue();
+        assertThat(sslParameters.getUseCipherSuitesOrder()).isTrue();
+        assertThat(sslParameters.getCipherSuites()).containsExactly("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+                "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+                "TLS_RSA_WITH_AES_128_GCM_SHA256",
+                "TLS_RSA_WITH_AES_128_CBC_SHA",
+                "TLS_RSA_WITH_AES_256_CBC_SHA",
+                "TLS_AES_128_GCM_SHA256",
+                "TLS_AES_256_GCM_SHA384",
+                "TLS_CHACHA20_POLY1305_SHA256");
+        assertThat(sslParameters.getServerNames()).isNull();
     }
 
     @Test
@@ -44,5 +107,29 @@ class OpenSslTest {
         assertThat(sessionCtx.getSessionCacheSize()).isEqualTo(20480);
         assertThat(sessionCtx.getSessionTimeout()).isEqualTo(300);
         assertThat(sessionCtx.isSessionCacheEnabled()).isEqualTo(true);
+        assertEquals(1, serverCtx.refCnt());
+        SslHandler handler = serverCtx.newHandler(ByteBufAllocator.DEFAULT);
+        assertNull(handler.applicationProtocol());
+        OpenSslEngine engine = (OpenSslEngine) handler.engine();
+        assertThat(engine.getEnabledProtocols()).containsExactly("SSLv2Hello", "TLSv1.2", "TLSv1.3");
+        assertThat(engine.getSupportedProtocols()).containsExactly("SSLv2Hello", "TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3");
+        SSLParameters sslParameters = engine.getSSLParameters();
+        assertThat(sslParameters.getNeedClientAuth()).isFalse();
+        assertThat(sslParameters.getWantClientAuth()).isFalse();
+        assertThat(sslParameters.getEnableRetransmissions()).isTrue();
+        assertThat(sslParameters.getUseCipherSuitesOrder()).isTrue();
+        assertThat(sslParameters.getCipherSuites()).containsExactly("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+                "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+                "TLS_RSA_WITH_AES_128_GCM_SHA256",
+                "TLS_RSA_WITH_AES_128_CBC_SHA",
+                "TLS_RSA_WITH_AES_256_CBC_SHA",
+                "TLS_AES_128_GCM_SHA256",
+                "TLS_AES_256_GCM_SHA384",
+                "TLS_CHACHA20_POLY1305_SHA256");
+        assertThat(sslParameters.getServerNames()).isNull();
     }
 }
