c5a6874023a162d61c452bb4a89e74b1c9034ddd
sullis <github@seansullivan.com>
2024-12-11 19:45:08 -0800

    netty 4.2.0 RC1

--- src/test/java/io/github/sullis/netty/playground/HttpContentCompressorTest.java
+++ src/test/java/io/github/sullis/netty/playground/HttpContentCompressorTest.java
@@ -2,25 +2,20 @@ package io.github.sullis.netty.playground;
 
 import io.netty.handler.codec.compression.CompressionOptions;
 import io.netty.handler.codec.http.HttpContentCompressor;
-import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Test;
-import java.lang.reflect.Field;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HttpContentCompressorTest {
-    private static final Field FIELD_SUPPORTS_COMPRESSION_OPTIONS = FieldUtils.getDeclaredField(HttpContentCompressor.class, "supportsCompressionOptions", true);
-
     @Test
-    void zeroArgConstructor_supportsCompressionOptionsIsFalse() throws Exception {
+    void zeroArgConstructor() throws Exception {
         HttpContentCompressor compressor = new HttpContentCompressor();
-        assertFalse(((Boolean) FIELD_SUPPORTS_COMPRESSION_OPTIONS.get(compressor)));
+        assertNotNull(compressor);
     }
 
     @Test
-    void compressorOptionsArrayConstructor_supportsCompressionOptionsIsTrue() throws Exception {
+    void compressorOptionsArrayConstructor() throws Exception {
         HttpContentCompressor compressor = new HttpContentCompressor((CompressionOptions[]) null);
-        assertTrue(((Boolean) FIELD_SUPPORTS_COMPRESSION_OPTIONS.get(compressor)));
+        assertNotNull(compressor);
     }
 }
