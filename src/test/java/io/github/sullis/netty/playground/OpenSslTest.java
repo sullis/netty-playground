bc9a8d2b6e772c180901060fc22b4c1657f8ea6f
sullis <github@seansullivan.com>
2025-03-08 18:52:54 -0800

    netty 4.2.0.RC4

--- src/test/java/io/github/sullis/netty/playground/OpenSslTest.java
+++ src/test/java/io/github/sullis/netty/playground/OpenSslTest.java
@@ -101,7 +101,7 @@ class OpenSslTest {
                 "TLS_AES_128_GCM_SHA256",
                 "TLS_AES_256_GCM_SHA384",
                 "TLS_CHACHA20_POLY1305_SHA256");
-        assertThat(sslParameters.getServerNames()).isNull();
+        assertThat(sslParameters.getServerNames()).isEmpty();
     }
 
     @Test
